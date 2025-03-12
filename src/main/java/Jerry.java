32f18dad5b0bd986a01f4e4429aa4a1804012356
ManZ9802 <wongmanzhong@gmail.com>
2025-03-12 22:04:06 +0800

    Added documentation for Jerry class

--- src/main/java/Jerry.java
+++ src/main/java/Jerry.java
@@ -1,3 +1,22 @@
+/**
+ * <h1> Jerry Chatbot</h1>
+ * <p>
+ * The Jerry chatbot implements a task tracker that tracks three types of
+ * tasks: Todo, Event, Deadline. </p>
+ * <p>
+ *     It takes in different commands as argument and returns an output
+ *     based on those commands.
+ * </p>
+ * <p>
+ *     When loading in, Jerry will read from a .txt file for any pre-existing list
+ *     and will save to the same file when exiting to preserve the list when the program is closed.
+ * </p>
+ * @author Man Zhong
+ * @version 1.0
+ * @since AY24/25 Semester 2
+ */
+
+
 public class Jerry {
     public static void main(String[] args) {
         Ui.run();
